4f46778c5e02500ed7c37606a5aa733ed7326856
Shnyakin Yaroslav <babyyod508@gmail.com>
2026-03-10 10:15:33 +0300

    Задали переменные

--- source/main.c
+++ source/main.c
@@ -1,7 +1,19 @@
 #include <studio.h>
 #include <stdint.h>
 
+#define CHIP8_MEMORY_SIZE	4096
+#define CHIP8_DISPLAY_WIDTH	64
+#define CHIP8_DISPLAY_HEIGHT	32
+#define V_REGISTER_COUNT	16
+#define STACK_DEPTH		16
+#define CHIP8_KEY_COUNT		16
+
+
 int main() {
+	typedef struct Chip8State {
+		int a;
+		int b;
+	};
 	printf("Hello World!");
 		return 0;
 }
